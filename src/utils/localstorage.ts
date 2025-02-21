193ff089dd60aeb64fa4bec7a1706fb3e7702bab
Gustavo Domingueti <gustavo.s.domingueti@icloud.com>
2025-02-20 21:44:14 -0300

    fix: check object window when is running on server side

--- src/utils/localstorage.ts
+++ src/utils/localstorage.ts
@@ -1,8 +1,16 @@
-export function setToLocalstorage<T>(key: string, value: T): void {
+export function setToLocalstorage<T>(key: string, value: T): void | null {
+  if (typeof window === 'undefined') {
+    return null;
+  }
+
   localStorage.setItem(key, JSON.stringify(value));
 }
 
 export function getFromLocalstorage<T>(key: string): T | null {
+  if (typeof window === 'undefined') {
+    return null;
+  }
+
   const storedItem = localStorage.getItem(key);
 
   if (!storedItem) {
